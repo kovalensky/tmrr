ed8920c59eea7fd75b7ab3c13810433d236768ce
Jacob <45331093+kovalensky@users.noreply.github.com>
2023-02-10 01:13:21 -0800

    New feature: Calculate individual merkle hashes of files

--- exe.php
+++ exe.php
@@ -1,4 +1,7 @@
 <?php
+// Constants for merkle calculation
+const BLOCK_SIZE = 16384;
+const HASH_SIZE = 32;
 // Bencode library
 function bencode_decode($input) {
     if ($input === '') {
@@ -67,10 +70,27 @@ function bencode_decode_r($input, $len, &$pos) {
 
 // Check for arguments
 
-if (isset($argv[1]) && isset($argv[2]) OR !isset($argv[1])) {
+if (!isset($argv[1])) {
 die("\r\nPlease use the correct syntax, as tmrr.exe <torrent-file-name> \r\n\r\n---\r\nDev by Tikva on Rutracker.org\r\nTelegram: @vatruski\r\n\r\nGreetings from the Russian Federation!");
 	}
+if ($argv[1] == "r" && isset($argv[2])) {
+// Do magic
+$file = $argv[2];
+	if(is_file($file)){
+	$root = new HasherV2($file, BLOCK_SIZE);
+	die( "Root hash of $file: " . bin2hex($root->root) );
+	}
+	else{
+		
+		die("This is not a raw file comrade");
+	}
+	
+	}
+if($argv[1] == "r" && !isset($argv[2]) ){
+		die("Please provide raw file location comrade");
+	}
 
+// Since no "r" argument key provided this has to be a torrent file, let's extract hashes
 	$torrent_file = @file_get_contents($argv[1]);
 	$decoded = @bencode_decode($torrent_file);
 		if(!isset($decoded["info"])){
@@ -98,3 +118,90 @@ function printArrayNames($array, $parent = "") {
 }
 
 printArrayNames($make);
+
+// Individual files Merkle computations
+function next_power_2($value) {
+    if (!($value & ($value - 1)) && $value) {
+        return $value;
+    }
+    $start = 1;
+    while ($start < $value) {
+        $start <<= 1;
+    }
+    return $start;
+}
+
+function merkle_root($blocks) {
+    if (count($blocks) > 0) {
+        while (count($blocks) > 1) {
+            for ($i = 0; $i < count($blocks); $i += 2) {
+                $x = $blocks[$i];
+                $y = isset($blocks[$i + 1]) ? $blocks[$i + 1] : '';
+                $blocks[$i / 2] = hash('sha256', $x . $y, true);
+            }
+            $blocks = array_slice($blocks, 0, count($blocks) / 2);
+        }
+        return $blocks[0];
+    }
+    return $blocks;
+}
+
+class HasherV2 {
+    private $path;
+    public $root;
+    private $piece_layer;
+    private $layer_hashes;
+    private $piece_length;
+    private $num_blocks;
+    public function __construct($path, $piece_length) {
+        $this->path = $path;
+        $this->root = null;
+        $this->piece_layer = null;
+        $this->layer_hashes = [];
+        $this->piece_length = $piece_length;
+        $this->num_blocks = $piece_length / BLOCK_SIZE;
+        $fd = fopen($this->path, 'rb');
+        $this->process_file($fd);
+        fclose($fd);
+    }
+
+    private function process_file($fd) {
+        while (!feof($fd)) {
+            $blocks = [];
+            $leaf = fread($fd, BLOCK_SIZE);
+            if (!$leaf) {
+                break;
+            }
+            $blocks[] = hash('sha256', $leaf, true);
+            if (count($blocks) != $this->num_blocks) {
+                $remaining = $this->num_blocks - count($blocks);
+                if (count($this->layer_hashes) == 0) {
+                    $power2 = next_power_2(count($blocks));
+                    $remaining = $power2 - count($blocks);
+                }
+                $padding = array_fill(0, $remaining, str_repeat("\x00", HASH_SIZE));
+                $blocks = array_merge($blocks, $padding);
+            }
+            $layer_hash = merkle_root($blocks);
+            $this->layer_hashes[] = $layer_hash;
+        }
+        $this->_calculate_root();
+    }
+	private function _calculate_root() {
+  $this->piece_layer = "";
+  foreach ($this->layer_hashes as $hash) {
+    $this->piece_layer .= $hash;
+  }
+
+  $hashes = count($this->layer_hashes);
+  if ($hashes > 1) {
+    $pow2 = next_power_2($hashes);
+    $remainder = $pow2 - $hashes;
+    $pad_piece = array_fill(0, $this->num_blocks, str_repeat("\x00", HASH_SIZE));
+    for ($i = 0; $i < $remainder; $i++) {
+      $this->layer_hashes[] = merkle_root($pad_piece);
+    }
+  }
+  $this->root = merkle_root($this->layer_hashes);
+}
+}
