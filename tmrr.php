5823ea1983b8c6067658459e2e22fb160f653e5d
Constantine Kovalensky <45331093+kovalensky@users.noreply.github.com>
2023-04-29 03:13:47 -0700

    Code reformatting

--- tmrr.php
+++ tmrr.php
@@ -342,7 +342,7 @@ $err_status =[];
 			}
 		}
 
-		if($filed > 0){
+		if(empty($dup_hashes)){
 			echo "\r\n " . $msg["no_duplicates"] . "\r\n\r\n" . $msg["total_files"] . ": $filec\r\n";
 		}
 		else{
