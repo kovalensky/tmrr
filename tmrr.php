18346a945fad40765cd9c9c2a62d5c54f3d58176
Constantine Kovalensky <45331093+kovalensky@users.noreply.github.com>
2023-07-26 12:33:18 +0400

    Minor improvements
    The size calculation function has been slightly optimized

--- tmrr.php
+++ tmrr.php
@@ -668,12 +668,11 @@ $msg = lang();
 		function formatBytes($bytes, $precision = 2)
 		{
 			$units = ['B', 'KB', 'MB', 'GB', 'TB'];
-			$bytes = max($bytes, 0);
+
 			$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
 			$pow = min($pow, count($units) - 1);
-			$bytes /= pow(1024, $pow);
 
-			return round($bytes, $precision) . ' ' . $units[$pow];
+			return round($bytes / (1024 ** $pow), $precision) . ' ' . $units[$pow];
 		}
 
 		// Format coloured text
